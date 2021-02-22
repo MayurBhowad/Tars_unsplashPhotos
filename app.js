7e4044f243a8773e2cdd3652fd0f57adfbff5afe
MayurBhowad <mayurbhowad1995@gmail.com>
2021-02-22 22:54:22 +0530

    initial commit

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
 
 if (process.env.NODE_ENV === 'production') {
     //set static folder
-    app.use(express.static('clsient/build'));
+    app.use(express.static('client/build'));
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
