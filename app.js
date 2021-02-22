a421ba45efd6c49f6eb167ed10cddd9070a7621b
MayurBhowad <mayurbhowad1995@gmail.com>
2021-02-22 22:32:54 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 // const mongoConnect = require('./config/db.config');
 // mongoConnect();
+
+app.use(cors())
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
